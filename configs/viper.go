e3975fa5bd59f511cd510123a88e8b143f7aebb1
Prio Arief Gunawan <prioariefgunawangunawan@gmail.com>
2024-01-03 18:18:17 +0700

    fix dockerizing go app

--- configs/viper.go
+++ configs/viper.go
@@ -11,18 +11,17 @@ func NewViper() *viper.Viper {
 	config := viper.New()
 
 	if err := godotenv.Load(); err != nil {
-		log.Fatal("Error loading .env file")
+		config.AutomaticEnv()
+	} else {
+		config.SetConfigName(".env")
+		config.SetConfigType("env")
+		config.AddConfigPath(".")
+
+		err := config.ReadInConfig()
+		if err != nil {
+			log.Fatalf("Error read config: %v", err)
+		}
 	}
-
-	config.AutomaticEnv()
-	// config.SetConfigName(".env")
-	// config.SetConfigType("env")
-	// config.AddConfigPath(".")
-
-	// err := config.ReadInConfig()
-	// if err != nil {
-	// 	log.Fatalf("Error read config: %v", err)
-	// }
 
 	return config
 }
